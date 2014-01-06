62d44ddb8c9749461bc4c620c1b8c537095dddb4
Johannes Wegener <mail@johanneswegener.de>
2014-01-06 10:45:32 +0100

    added golay generator and IR generator

---
+++ golay.py
@@ -0,0 +1,32 @@
+# Golay Generation Based on the realsimp Project at the Stanford University
+# Source Code can be found at:
+#	https://ccrma.stanford.edu/realsimple/imp_meas/golay_response.m
+#	https://ccrma.stanford.edu/realsimple/imp_meas/generate_golay.m
+
+def generate_golay(N):
+	""" Generate the Golay codes a and b with length 2**N """
+	# These initial a and b values are Golay
+	a = [1 1]
+	b = [1 -1]
+
+	# Iterate to create a longer Golay sequence
+	while N>1:
+		olda = a
+		oldb = b
+		a = [olda oldb]
+		b = [olda -oldb]
+
+		N -= 1
+
+	return a, b
+
+from scipy.signal import fftconvolve
+import numpy as np
+def golayIR(respa, respb, a, b):
+	# Comptute impulse response h
+	L = len(a)
+	h = np.array(zeros(respa.shape))
+	for i in range(0,respa.shape[0]):
+		h[i] = fftconvolve(a[-1::-1],respa[i])+fftconvolve(b[-1::-1], respb[i])
+		h[i] = h[i] / (2*L)
+	return h
