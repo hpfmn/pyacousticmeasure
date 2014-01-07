f3a15f32029d2f2a52b005bf04ba00b14cb7ec39
Johannes Wegener <mail@johanneswegener.de>
2014-01-07 02:17:25 +0100

    mls with taps and working

--- mls.py
+++ mls.py
@@ -3,30 +3,36 @@
 import numpy as np
 from numpy.random import rand
 
+# Taps from Table 1 in JOHN VANDERKOOY: Aspects of MLS Measuring Systems
+
+tap1=range(2,33)
+tap2=[1,1,1,2,1,1,6,4,7,2,11,4,13,14,5,3,7,6,3,2,1,5,4,3,8,8,3,2,16,3,28]
+tap3=[0,0,0,0,0,0,5,0,0,0,10,3,12, 0,3,0,0,5,0,0,0,0,3,0,7,7,0,0,15,0,27]
+tap4=[0,0,0,0,0,0,1,0,0,0, 2,1, 2, 0,2,0,0,1,0,0,0,0,1,0,1,1,0,0, 1,0, 1]
+
 def generate_mls(n,flag):
 	# Initilize buffer
 	if flag == 1:
-		abuff = np.ones(1,n)
+		abuff = np.ones(n,dtype=np.int32)
 	else:
-		np.rand(1,n)
 		while True:
-			abuff = np.round(rand(1,n))
+			abuff = np.array(np.round(rand(n)),dtype=np.int32)
 			# be shure not all bits are zero
 			if 1 in abuff:
 				break
 	# initilize output
-	y=np.zeros(2**n-1)
+	y=np.zeros(2**n-1,dtype=np.int32)
 	# shift register loop
-	for i in range((0,int((2**n)-1)):
-		xorbit = abuff[tap1]^abuff[tap2]
+	for i in range(1,int((2**n))):
+		xorbit = abuff[tap1[n-2]-1]^abuff[tap2[n-2]-1]
 
-		if taps==4:
-			xorbit2 = xorbit^abuff[tap3]
-			xorbit3 = xorbit2^abuff[tap4]
-			abuff=np.insert(abuff,xorbit3,0)
+		if (tap3[n-2]+tap4[n-2])>0:
+			xorbit2 = xorbit^abuff[tap3[n-2]-1]
+			xorbit3 = xorbit2^abuff[tap4[n-2]-1]
+			abuff=np.insert(abuff,0,xorbit3)
 		else:
-			abuff=np.insert(abuff,xorbit,0)
-		y[i]=abuff[-1]
+			abuff=np.insert(abuff,0,xorbit)
+		y[i-1]=int((-2*abuff[-1])+1)
 		abuff=np.delete(abuff,-1)
-
+	return y
 
